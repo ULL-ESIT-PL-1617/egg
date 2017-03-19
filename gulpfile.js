8497b903b95c0b78da3c13d44b95fbf4f7d40078
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-19 10:58:16 +0000

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('debug', shell.task('node --inspect --debug-brk egg.js')
+gulp.task('debug', shell.task('node --inspect --debug-brk main.js')
 );
