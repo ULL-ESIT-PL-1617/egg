88fe2764f94afd172c6f8fcd2700a7c7048d9a4e
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-19 10:51:58 +0000

    separated code

---
+++ main.js
@@ -0,0 +1,19 @@
+var run = require('eggvm');
+
+// Examples
+run("do(define(x, 4),",
+    "   define(setx, fun(val, set(x, val))),",
+    "   setx(50),",
+    "   print(x))");
+
+run("do(define(sum, fun(array,",
+    "     do(define(i, 0),",
+    "        define(sum, 0),",
+    "        while(<(i, length(array)),",
+    "          do(define(sum, +(sum, element(array, i))),",
+    "             define(i, +(i, 1)))),",
+    "        sum))),",
+    "   print(sum(array(1, 2, 3))))");
+
+
+
