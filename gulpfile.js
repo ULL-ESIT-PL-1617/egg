1e2e905c0741b590528f6c3152470ab3fb737efa
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-23 17:52:06 +0000

    .travis.yml

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('debug', shell.task('node --inspect --debug-brk main.js')
-);
+gulp.task("default", ["test"]);
+
+gulp.task('debug', shell.task('node --inspect --debug-brk main.js'));
 
 gulp.task('run', shell.task('node main.js'));
 
