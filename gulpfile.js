01bf970809e31d26d304867699c0555fb8c9f075
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-14 16:10:08 +0000

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var shell = require('gulp-shell');
 gulp.task("default", ["test"]);
 
 gulp.task('debug', shell.task('node --inspect --debug-brk main.js'));
-gulp.task('deb', shell.task('node inspect main.js'));
+gulp.task('deb', shell.task('node --inspect-brk main.js'));
 
 gulp.task('run', shell.task('node main.js'));
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
+
+gulp.task("publish", shell.task("npm publish --access public"));
