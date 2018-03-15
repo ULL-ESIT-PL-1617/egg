6ee9d5c3b7877fc20d83d6d08ae108101c19cbd1
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-15 14:33:09 +0000

    simplifying

--- parse.js
+++ parse.js
@@ -4,6 +4,7 @@ const NUMBER = /^\s*(\d+)\b\s*/;
 const WORD   = /^\s*([^\s(),"]+)\s*/;
 const LP     = /^\s*([(])\s*/;
 const RP     = /^\s*([)])\s*/;
+const COMMA  = /^\s*(,)\s*/;
 
 function parseExpression(program) {
   //program = skipSpace(program);
@@ -27,34 +28,37 @@ function skipSpace(string) {
 }
 
 function parseApply(expr, program) {
-  program = skipSpace(program);
+  var match, rpmatch;
+  //program = skipSpace(program);
 
   if (!(match = LP.exec(program))) { // no apply
     return {expr: expr, rest: program};
   }
 
-  program = skipSpace(program.slice(match[0].length));
+  //program = skipSpace(program.slice(match[0].length));
+  program = (program.slice(match[0].length));
   expr = {type: 'apply', operator: expr, args: []};
 
-  while (program[0] != ')') {
+  while (!(rpmatch = RP.exec(program))) {
     var arg = parseExpression(program);
     expr.args.push(arg.expr);
-    program = skipSpace(arg.rest);
+    program = arg.rest;
 
-    if (program[0] == ',') {
-      program = skipSpace(program.slice(1));
-    } else if (program[0] != ')') {
+    if (match = COMMA.exec(program)) {
+      program = (program.slice(match[0].length));
+    } else if (!(RP.exec(program))) {
       throw new SyntaxError("Expected ',' or ')'");
     }
   }
 
-  return parseApply(expr, program.slice(1));
+  return parseApply(expr, program.slice(rpmatch[0].length));
 }
 
 function parse(program) {
   var result = parseExpression(program);
 
   if (skipSpace(result.rest).length > 0) {
+  //if ((result.rest).length > 0) {
     throw new SyntaxError('Unexpected text after program');
   }
 
