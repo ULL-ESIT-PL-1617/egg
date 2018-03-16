04153d8fe5edb1968e92bb044a649d0ebedbbed2
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-16 16:01:48 +0000

    with lexical analyzer. working!

--- parse.js
+++ parse.js
@@ -1,9 +1,11 @@
+const inspect = require("util").inspect;
+
 const WHITES = /^(\s|#.*)*/;
 const STRING = /^\s*"([^"]*)"\s*/;
 const NUMBER = /^\s*(\d+)\b\s*/;
 const WORD   = /^\s*([^\s(),"]+)\s*/;
 const LP     = /^\s*([(])\s*/;
-const RP     = /^\s*([)])\s*/;
+const RP     = /^\)/;
 const COMMA  = /^\s*(,)\s*/;
 
 let program;
@@ -15,71 +17,77 @@ function lex() {
     match = WHITES.exec(program);
     program = program.slice(match[0].length);
 
-    program = program.replace(WHITES,'');
-    if (match = STRING.exec(program)) {
-      lookahead = { type: "STRING", value: match[1]}
-    } else if (match = NUMBER.exec(program)) {
-      lookahead = {type: "NUMBER", value: Number(match[1])};
-    } else if (match = WORD.exec(program)) {
-      lookahead = {type: "WORD", value: match[1]};
-    } else if (match = LP.exec(program)) { 
-      lookahead = {type: "LP", value: match[1]};
-    } else if (match = COMMA.exec(program)) {
-      lookahead = {type: "COMMA", value: match[1]};
-    } else if (RP.exec(program)) {
-      lookahead = {type: "RP", value: match[1]};
-    } else {
-      throw new SyntaxError(`Unexpect syntax: ${program}`);
+    if (program.length > 0) {
+      if (match = STRING.exec(program)) {
+        lookahead = { type: "STRING", value: match[1]}
+      } else if (match = NUMBER.exec(program)) {
+        lookahead = {type: "NUMBER", value: Number(match[1])};
+      } else if (match = LP.exec(program)) { 
+        lookahead = {type: "LP", value: match[1]};
+      } else if (match = COMMA.exec(program)) {
+        lookahead = {type: "COMMA", value: match[1]};
+      } else if (match = RP.exec(program)) {
+        lookahead = {type: "RP", value: match[0]};
+      } else if (match = WORD.exec(program)) {
+        lookahead = {type: "WORD", value: match[1]};
+      } else {
+        throw new SyntaxError(`Unexpect syntax: ${program.slice(0,10)}`);
+      }
+      program = program.slice(match[0].length);
     }
-    program = program.slice(match[0].length);
+    else {
+      debugger;
+      lookahead = null;
+    }
     return lookahead;
 }
 
-  function parseExpression() {
-    var match, expr;
+function parseExpression() {
+  var expr;
 
-    program = program.replace(WHITES,'');
-    if (match = STRING.exec(program)) {
-      expr = {type: "value", value: match[1]};
-    } else if (match = NUMBER.exec(program)) {
-      expr = {type: "value", value: Number(match[1])};
-    } else if (match = WORD.exec(program)) {
-      expr = {type: "word", name: match[1]};
-    } else {
-      throw new SyntaxError(`Unexpect syntax: ${program}`);
-    }
-    program = program.slice(match[0].length);
-    return parseApply(expr);
+  if (lookahead.type == "STRING") {
+    expr = {type: "value", value: lookahead.value};
+  } else if (lookahead.type == "NUMBER") {
+    expr = {type: "value", value: lookahead.value};
+  } else if (lookahead.type == "WORD") {
+    expr = {type: "word", name: lookahead.value};
+  } else {
+    throw new SyntaxError(`Unexpected syntax: ${program.slice(0,10)}`);
+  }
+  lex();
+  return parseApply(expr);
+}
+
+function parseApply(tree) {
+
+  if (!lookahead) return tree;
+
+  if (lookahead.type !== "LP") { // no apply
+    return tree;
   }
 
-  function parseApply(tree) {
-    var match, rpmatch;
+  lex();
+  tree = {type: 'apply', operator: tree, args: []};
 
-    program = program.replace(WHITES,'');
-    if (!(match = LP.exec(program))) { // no apply
-      return tree;
+  while (lookahead.type !== "RP") {
+    var arg = parseExpression();
+    tree.args.push(arg);
+
+    if (lookahead.type == "COMMA") {
+      lex();
+    } else if (lookahead.type !== "RP") {
+      throw new SyntaxError("Expected ',' or ')'");
     }
-
-    program = program.slice(match[0].length);
-    tree = {type: 'apply', operator: tree, args: []};
-
-    while (!(rpmatch = RP.exec(program))) {
-      var arg = parseExpression();
-      tree.args.push(arg);
-
-      if (match = COMMA.exec(program)) {
-        program = (program.slice(match[0].length));
-      } else if (!(RP.exec(program))) {
-        throw new SyntaxError("Expected ',' or ')'");
-      }
-    }
-    program = program.slice(rpmatch[0].length);
-    return parseApply(tree);
   }
+  lex();
+  return parseApply(tree);
+}
 
 function parse(p) {
   program = p;
+  lex();
   var result = parseExpression();
+  //console.log("result = ",inspect(result, {depth: null}));
 
   return result;
 }
