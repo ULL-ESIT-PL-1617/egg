fc8815746a353de4f3dce7c59124ca16b3df7d7a
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-16 16:41:33 +0000

    one-err-2.egg

--- parse.js
+++ parse.js
@@ -1,20 +1,25 @@
 const inspect = require("util").inspect;
 
 const WHITES = /^(\s|#.*)*/;
-const STRING = /^\s*"([^"]*)"\s*/;
-const NUMBER = /^\s*(\d+)\b\s*/;
-const WORD   = /^\s*([^\s(),"]+)\s*/;
-const LP     = /^\s*([(])\s*/;
+const STRING = /^"([^"]*)"/;
+const NUMBER = /^(\d+)\b/;
+const WORD   = /^([^\s(),"]+)/;
+const LP     = /^([(])/;
 const RP     = /^\)/;
-const COMMA  = /^\s*(,)\s*/;
+const COMMA  = /^(,)/;
+const NEWLINE = /\r\n|\r|\n/;
 
 let program;
 let lookahead;
+let lineno = 1;
 
 function lex() {
     let match;
 
     match = WHITES.exec(program);
+    if (NEWLINE.test(match[0])) {
+      lineno += match[0].split(NEWLINE).length-1;
+    }
     program = program.slice(match[0].length);
 
     if (program.length > 0) {
@@ -31,12 +36,11 @@ function lex() {
       } else if (match = WORD.exec(program)) {
         lookahead = {type: "WORD", value: match[1]};
       } else {
-        throw new SyntaxError(`Unexpect syntax: ${program.slice(0,10)}`);
+        throw new SyntaxError(`Unexpect syntax line ${lineno}: ${program.slice(0,10)}`);
       }
       program = program.slice(match[0].length);
     }
     else {
-      debugger;
       lookahead = null;
     }
     return lookahead;
@@ -52,7 +56,7 @@ function parseExpression() {
   } else if (lookahead.type == "WORD") {
     expr = {type: "word", name: lookahead.value};
   } else {
-    throw new SyntaxError(`Unexpected syntax: ${program.slice(0,10)}`);
+    throw new SyntaxError(`Unexpected syntax line ${lineno}: ${program.slice(0,10)}`);
   }
   lex();
   return parseApply(expr);
@@ -67,19 +71,23 @@ function parseApply(tree) {
   }
 
   lex();
+
   tree = {type: 'apply', operator: tree, args: []};
 
-  while (lookahead.type !== "RP") {
+  while (lookahead && lookahead.type !== "RP") {
     var arg = parseExpression();
     tree.args.push(arg);
 
-    if (lookahead.type == "COMMA") {
+    if (!lookahead)  debugger;
+    if (lookahead && lookahead.type == "COMMA") {
       lex();
-    } else if (lookahead.type !== "RP") {
-      throw new SyntaxError("Expected ',' or ')'");
+    } else if (!lookahead || lookahead.type !== "RP") {
+      throw new SyntaxError(`Expected ',' or ')'  at line ${lineno}: ... ${program.slice(0,20)}`);
     }
   }
   lex();
+
+  if (!lookahead) return tree;
   return parseApply(tree);
 }
 
