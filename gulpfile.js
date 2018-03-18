4b9d1aa5657cae391ca1dbc569511dc3c67460be
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-18 11:21:27 +0000

    test-package added to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,15 @@ gulp.task('run', shell.task('node ./egg.js one.egg'));
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
 
+gulp.task("test-package", shell.task(
+[
+  "rm -fR /tmp/check-egg && "+
+  "git clone git@gist.github.com:8dfcaa01a0377dead374bc35c462c29d.git /tmp/check-egg && "+ 
+  "cd /tmp/check-egg/ && "+
+  "npm i --no-save && "+
+  "npm start | grep '395\.5'"
+]));
+
 gulp.task("publish", shell.task([
   "npm version patch",
   "npm publish --access public"
