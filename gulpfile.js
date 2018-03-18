e834516b2351c2399e1d3cda468f179be5cd1caa
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-18 11:29:44 +0000

    reorganizing

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var shell = require('gulp-shell');
 
 gulp.task("default", ["test"]);
 
-gulp.task('deb', shell.task('node --inspect-brk ./egg.js one.egg'));
+gulp.task('deb', shell.task('node --inspect-brk ./egg.js examples/one.egg'));
 
-gulp.task('run', shell.task('node ./egg.js one.egg'));
+gulp.task('run', shell.task('node ./egg.js examples/one.egg'));
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
 
