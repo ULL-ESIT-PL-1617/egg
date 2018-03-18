be79b19455ca12aca32445526ceb3284f279fcdd
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-18 11:41:28 +0000

    restructuring

--- bin/evm.js
+++ bin/evm.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var {runFromEVM}  = require('./eggvm.js');
+var {runFromEVM}  = require('../eggvm.js');
 
 const fileName = process.argv.slice(2).shift();
 if (fileName && fileName.length > 0) runFromEVM(fileName);
--- eggparse.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-var {parseFromFile}  = require('./parse.js');
-
-const fileName = process.argv.slice(2).shift();
-if (fileName && fileName.length > 0) parseFromFile(fileName);
-
-
